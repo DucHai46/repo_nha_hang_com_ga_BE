a2513faa91200122c8183f39414fe93ecff841cd
DucHai46 <b.duc.hai2003@gmail.com>
2025-05-25 23:04:12 +0700

    fix bug

--- auth.service/Controllers/AuthController.cs
+++ auth.service/Controllers/AuthController.cs
@@ -285,9 +285,9 @@ public class AuthController : Controller
 
 public class UpdateUserModel
 {
-    public string Username { get; set; }
-    public string OldPassword { get; set; }
-    public string Password { get; set; }
+    public string? Username { get; set; }
+    public string? OldPassword { get; set; }
+    public string? Password { get; set; }
 }
 
 public class RegisterModel
